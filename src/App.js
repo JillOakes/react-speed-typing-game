edd3524541b03077d975c905d4467d6617191910
Jill Oakes <jill.oakes@beckshybrids.com>
2022-08-10 12:08:30 -0400

    Add useRef, useContext, useGame

--- src/App.js
+++ src/App.js
@@ -1,52 +1,30 @@
 import React from "react";
+import useGame from "./useGame";
 
 function App() {
-  const STARTING_TIME = 5
-
-  const [text, setText] = React.useState("");
-  const [timeRemaining, setTimeRemaining] = React.useState(STARTING_TIME);
-  const [isTimeRunning, setIsTimeRunning] = React.useState(false);
-  const [wordCount, setWordCount] = React.useState(0);
-
-  function handleChange(e) {
-    const { value } = e.target;
-    setText(value);
-  }
-
-  function calculateWordCount(text) {
-    const wordsArr = text.trim().split(" ");
-    const filteredWords = wordsArr.filter((word) => word !== "");
-    console.log(filteredWords.length);
-    return filteredWords.length;
-  }
-
-  function startGame() {
-    setIsTimeRunning(true);
-    setTimeRemaining(STARTING_TIME);
-    setText("");
-  }
-
-  function endGame() {
-    setIsTimeRunning(false);
-    setWordCount(calculateWordCount(text));
-  }
-
-  React.useEffect(() => {
-    if (isTimeRunning && timeRemaining > 0) {
-      setTimeout(() => {
-        setTimeRemaining((time) => time - 1);
-      }, 1000);
-    } else if (timeRemaining === 0) {
-      endGame();
-    }
-  }, [timeRemaining, isTimeRunning]);
+  const {
+    textBoxRef,
+    handleChange,
+    text,
+    isTimeRunning,
+    timeRemaining,
+    startGame,
+    wordCount,
+  } = useGame(5);
 
   return (
     <div>
       <h1>How fast do you type?</h1>
-      <textarea onChange={handleChange} value={text} disabled={!isTimeRunning} />
+      <textarea
+        ref={textBoxRef}
+        onChange={handleChange}
+        value={text}
+        disabled={!isTimeRunning}
+      />
       <h4>Time remaining: {timeRemaining}</h4>
-      <button onClick={startGame} disabled={isTimeRunning}>Start</button>
+      <button onClick={startGame} disabled={isTimeRunning}>
+        Start
+      </button>
       <h1>Word count: {wordCount}</h1>
     </div>
   );
